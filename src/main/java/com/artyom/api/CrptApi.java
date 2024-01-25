741f332e0a239827b8691ccc4926748174915d57
Artyom Hack <xakimullin2@gmail.com>
2024-01-26 00:16:54 +0300

    Разбираюсь в задаче. Добавил Executors

--- src/main/java/com/artyom/api/CrptApi.java
+++ src/main/java/com/artyom/api/CrptApi.java
@@ -13,40 +13,57 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.sql.Date;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 
+@Getter
 public class CrptApi {
     private static final ObjectMapper mapper = new ObjectMapper();
     private static final String URL = "https://ismp.crpt.ru/api/v3/lk/documents/create";
     private final Long limitMs;
-
-    private volatile long startPeriodMs = 0;
-
+    private final TimeUnit periodTimeUnit;
+    private final int requestLimit;
+    private final AtomicLong counts = new AtomicLong(0);
+    private final AtomicLong countRequest = new AtomicLong(0);
     private final Semaphore semaphore;
+    private final ScheduledExecutorService scheduledExecutor;
 
     public CrptApi(int requestLimit, long periodLimit, TimeUnit periodTimeUnit) {
+        this.requestLimit = requestLimit;
+        this.periodTimeUnit = periodTimeUnit;
         this.limitMs = periodTimeUnit.toMillis(periodLimit);
         semaphore = new Semaphore(requestLimit);
+        scheduledExecutor = new ScheduledThreadPoolExecutor(requestLimit);
     }
 
-    public void createDocument(Document document) throws InterruptedException {
-        if (startPeriodMs == 0)
-            startPeriodMs = System.currentTimeMillis();
-
-        semaphore.acquire();
-
-        sendRequest(document);
-
-        releaseSemaphore();
+    public AtomicLong createDocument(Document document) {
+        scheduledExecutor.scheduleWithFixedDelay(() -> {
+            try {
+                semaphore.acquire();
+                sendRequest(document);
+                countRequest.getAndIncrement();
+                counts.getAndAdd(countRequest.get());
+                System.out.println("Atomic count: " +  counts.get());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (countRequest.get() > requestLimit) {
+                    countRequest.set(0);
+                    semaphore.drainPermits();
+                } else
+                    semaphore. release();
+            }
+        }, 0, limitMs, periodTimeUnit);
+        return counts;
     }
 
-    private void sendRequest(Document document) {
+    public void sendRequest(Document document) {
         try {
             HttpURLConnection urlConnection = (HttpURLConnection) new URL(URL).openConnection();
             urlConnection.setRequestMethod("POST");
@@ -63,33 +80,6 @@ public class CrptApi {
         var writer = new OutputStreamWriter(stream);
         writer.write(json);
         writer.flush();
-    }
-
-    public void releaseSemaphore() {
-        if (semaphore.availablePermits() > 0) {
-            semaphore.release();
-        }
-
-        if (startPeriodMs > System.currentTimeMillis()) {
-            resetSemaphore();
-        }
-
-        if (semaphore.availablePermits() == 0) {
-           new Thread(() -> {
-                try {
-                    Thread.sleep(System.currentTimeMillis() - startPeriodMs);
-                    resetSemaphore();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }).start();
-        }
-    }
-
-    private void resetSemaphore() {
-        semaphore.release();
-        semaphore.drainPermits();
-        startPeriodMs = System.currentTimeMillis();
     }
 
     @AllArgsConstructor
@@ -137,40 +127,33 @@ public class CrptApi {
 
 class CrptApiTest {
 
-    private CrptApi crptApi  = new CrptApi(10, 1, TimeUnit.SECONDS);
+    private final CrptApi.Document.Description description = new CrptApi.Document.Description("participantInn");
+    private final List<CrptApi.Document.Product> products = List.of(new CrptApi.Document.Product(
+            "certificateDocument", Date.valueOf("2020-01-23"), "certificateDocumentNumber",
+            "ownerInn", "producerInn", Date.valueOf("2020-01-23"), "tnvedCode","uitCode", "uituCode"
+    ));
+    private final CrptApi.Document document =  new CrptApi.Document(
+                        description, "docId", "docStatus", "docType",
+            true, "ownerInn", "participantInn", "producerInn",
+            Date.valueOf("2020-01-23"), "productionType", products,
+                  Date.valueOf("2020-01-23"), "regNumber"
+    );
+
+    private CrptApi crptApi  = new CrptApi(5, 1, TimeUnit.SECONDS);
 
     @Test
     void sendLimitedRequest() throws InterruptedException {
         var start = System.currentTimeMillis();
-        var current = System.currentTimeMillis();
-        var end = current + 50000;
-        System.out.printf("Started at: %s\n", toLocalDateTime(current).toLocalTime());
-        var countRequests = 0;
-        while (current < end) {
-            crptApi.createDocument(createRandomDocument());
-            countRequests = countRequests + 1;
-            current+=1000;
-            System.out.printf("Sent request %d at: %s\n", countRequests, toLocalDateTime(current).toLocalTime());
+        var end = start + 5000;
+        System.out.printf("Started at: %s\n", LocalTime.now());
+        long countRequests = 0;
+        while (end > System.currentTimeMillis()) {
+            countRequests = crptApi.createDocument(document).get();
         }
-        System.out.println("All time: " + (end - start) / 10000 + " ms");
-        System.out.printf("Ended at: %s", toLocalDateTime(System.currentTimeMillis()).toLocalTime());
-        Assertions.assertEquals(50, countRequests);
-    }
-
-    private LocalDateTime toLocalDateTime(long ms) {
-        var instance = Instant.ofEpochMilli(ms);
-        return LocalDateTime.ofInstant(instance, ZoneId.systemDefault());
-    }
-
-    private CrptApi.Document createRandomDocument() {
-        return new CrptApi.Document(new CrptApi.Document.Description("participantInn"), "docId", "docStatus",
-                "docType", true, "ownerInn", "participantInn", "producerInn",
-                Date.valueOf("2020-01-23"), "productionType",
-                List.of(new CrptApi.Document.Product("certificateDocument",
-                        Date.valueOf("2020-01-23"), "certificateDocumentNumber",
-                        "ownerInn", "producerInn", Date.valueOf("2020-01-23"),
-                        "tnvedCode", "uitCode", "uituCode"
-                )), Date.valueOf("2020-01-23"), "regNumber");
+        System.out.println("All time: " + (end - start) / 1000 + " s");
+        System.out.printf("Ended at: %s", LocalTime.now());
+        System.out.println(countRequests);
+        Assertions.assertEquals(25, countRequests);
     }
 }
 
---
+++ src/test/java/com/artyom/api/CrptApiTest.java
@@ -0,0 +1,10 @@
+package com.artyom.api;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
